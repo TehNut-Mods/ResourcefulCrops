e75a783997bf6868ac68678fc9f66fd9a6271862
TehNut <nick_ignoffo@yahoo.com>
2015-06-28 23:04:18 -0700

    New ore and (finally) dust textures

--- src/main/java/tehnut/resourceful/crops/item/ItemMaterial.java
+++ src/main/java/tehnut/resourceful/crops/item/ItemMaterial.java
@@ -2,10 +2,12 @@ package tehnut.resourceful.crops.item;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import tehnut.resourceful.crops.ModInformation;
 import tehnut.resourceful.crops.ResourcefulCrops;
 
@@ -15,12 +17,12 @@ import java.util.List;
 public class ItemMaterial extends Item {
 
     String[] materials = { "", ".mundane", ".magical", ".infused", ".arcane" };
+    IIcon[] icons = new IIcon[materials.length];
 
     public ItemMaterial() {
         super();
 
         setUnlocalizedName(ModInformation.ID + ".dust");
-        setTextureName(ModInformation.ID + ":dust_base");
         setCreativeTab(ResourcefulCrops.tabResourcefulCrops);
         setHasSubtypes(true);
     }
@@ -37,29 +39,21 @@ public class ItemMaterial extends Item {
             list.add(new ItemStack(this, 1, i));
     }
 
+    @SideOnly(Side.CLIENT)
+    public IIcon getIconFromDamage(int meta) {
+        return icons[meta];
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister register) {
+        for (int i = 0; i < materials.length; i++)
+            icons[i] = register.registerIcon(ModInformation.ID + ":dust_" + i);
+    }
+
     @Override
     @SideOnly(Side.CLIENT)
     public boolean hasEffect(ItemStack stack, int pass) {
         return stack.getItemDamage() == 4;
-    }
-
-    @SideOnly(Side.CLIENT)
-    @Override
-    public int getColorFromItemStack(ItemStack stack, int pass) {
-        if (pass == 1)
-            return new Color(0, 151, 30).getRGB();
-        else
-            return super.getColorFromItemStack(stack, pass);
-    }
-
-    @Override
-    public int getRenderPasses(int metadata) {
-        return requiresMultipleRenderPasses() ? 2 : 1;
-    }
-
-    @SideOnly(Side.CLIENT)
-    public boolean requiresMultipleRenderPasses() {
-        return true;
     }
 
     @Override
