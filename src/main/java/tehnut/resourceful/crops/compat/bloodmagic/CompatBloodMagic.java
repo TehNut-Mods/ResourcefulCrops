55dca11a962a24c424f2c776ef19cedb9ad9b3b7
TehNut <nick_ignoffo@yahoo.com>
2015-07-14 21:10:59 -0700

    For future BM compat use

--- src/main/java/tehnut/resourceful/crops/compat/bloodmagic/CompatBloodMagic.java
+++ src/main/java/tehnut/resourceful/crops/compat/bloodmagic/CompatBloodMagic.java
@@ -7,4 +7,23 @@ public class CompatBloodMagic {
     static {
         HarvestRegistry.registerHarvestHandler(new ResourcefulCropsHarvestHandler());
     }
+
+    public enum OrbType {
+
+        WEAK(5000),
+        APPRENTICE(25000),
+        MAGICIAN(150000),
+        MASTER(1000000),
+        ARCHMAGE(10000000);
+
+        private int capacity;
+
+        OrbType(int capacity) {
+            this.capacity = capacity;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+    }
 }
