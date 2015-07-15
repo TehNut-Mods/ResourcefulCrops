578c5d3cf4db482d0cc2fae3cc1b29ceaf3ca809
TehNut <nick_ignoffo@yahoo.com>
2015-07-15 15:22:50 -0700

    Force a requirement on Forge 10.13.3.1374 and higher
    AKA: The version that added the super useful `OreDictionary.doesOreNameExist(String)` method.

--- src/main/java/tehnut/resourceful/crops/ModInformation.java
+++ src/main/java/tehnut/resourceful/crops/ModInformation.java
@@ -5,7 +5,7 @@ public class ModInformation {
     public static final String NAME = "Resourceful Crops";
     public static final String ID = "ResourcefulCrops";
     public static final String VERSION = "@VERSION@";
-    public static final String REQUIRED = "";
+    public static final String REQUIRED = "required-after:Forge@[10.13.3.1374,)";
     public static final String GUIFACTORY = "tehnut.resourceful.crops.client.gui.ConfigGuiFactory";
     public static final String CLIENTPROXY = "tehnut.resourceful.crops.proxy.ClientProxy";
     public static final String COMMONPROXY = "tehnut.resourceful.crops.proxy.CommonProxy";
