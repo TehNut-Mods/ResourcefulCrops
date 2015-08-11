9b3060197e39450e9651b29b0e6eba88786293af
Saereth <guiken@gmail.com>
2015-08-11 10:45:27 -0400

    Added configuration option to disallow AutoMiners

--- src/main/java/tehnut/resourceful/crops/block/BlockROre.java
+++ src/main/java/tehnut/resourceful/crops/block/BlockROre.java
@@ -6,13 +6,21 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.FakePlayer;
 import tehnut.resourceful.crops.api.ModInformation;
+import tehnut.resourceful.crops.ConfigHandler;
 import tehnut.resourceful.crops.ResourcefulCrops;
+import tehnut.resourceful.crops.registry.BlockRegistry;
 import tehnut.resourceful.crops.registry.ItemRegistry;
 
 import java.util.List;
@@ -44,14 +52,72 @@ public class BlockROre extends Block {
     @SideOnly(Side.CLIENT)
     public IIcon getIcon(int side, int meta) {
         return this.icons[meta];
+    
     }
+    
+       
 
+    @Override
+    public void harvestBlock(World world, EntityPlayer player, int x, int y, int z, int meta){
+    	int iQuantity = 1;
+    	int fortuneLevel = 0;
+    	if (player instanceof FakePlayer && ConfigHandler.enableFakePlayerMining != true){
+    		//This is a fake player, do nothing
+    		return;
+    	}
+    	else{	
+    		//This is an actual player, generate items based on enchantment
+    		System.out.println( "This is not a fake miner" );
+    		boolean silk = EnchantmentHelper.getSilkTouchModifier(player);
+    		if (silk) {
+    			Block blockItem = BlockRegistry.ore;
+    			BlockROre.dropItem(world, x, y, z, new ItemStack(blockItem,1,0));
+    		}
+
+    		else {
+    			fortuneLevel = EnchantmentHelper.getFortuneModifier(player);
+
+    			if (fortuneLevel > 0) {
+    				int bonus = random.nextInt(fortuneLevel + 2) - 1;
+
+    				if (bonus < 0)
+    					bonus = 0;
+
+    				iQuantity = 1  * (bonus + 1);
+    			} else {
+    				iQuantity = 1;
+    			}
+
+    			Item droppedItem = ItemRegistry.material;
+    			System.out.println( "Not silktouch, dropped items" );
+    			BlockROre.dropItem(world, x, y, z, new ItemStack(droppedItem,iQuantity,0));
+    		}
+    	}
+    	return;
+    }
+    
+	public static EntityItem dropItem(World world, double x, double y, double z, ItemStack iStack) {
+		final Random rand = new Random();
+		if (iStack == null)
+			return null;
+		EntityItem eItem = new EntityItem(world, x, y, z, iStack.copy());
+		eItem.delayBeforeCanPickup = 10;
+		eItem.motionX = (-0.1+0.2*rand.nextDouble()*.5);
+		eItem.motionZ = (-0.1+0.2*rand.nextDouble()*.5);
+		eItem.motionY = (0.2*rand.nextDouble()*.5);
+		if (!world.isRemote && world.getGameRules().getGameRuleBooleanValue("doTileDrops") && !world.restoringBlockSnapshots) {
+			world.spawnEntityInWorld(eItem);
+		}
+		return eItem;
+	}
+    
     @SuppressWarnings("unchecked")
     @Override
     @SideOnly(Side.CLIENT)
     public void getSubBlocks(Item id, CreativeTabs tab, List list) {
         for (int i = 0; i < 2; i++)
             list.add(new ItemStack(id, 1, i));
+    
     }
 
     @Override
@@ -60,7 +126,10 @@ public class BlockROre extends Block {
     }
 
     public Item getItemDropped(int damage, Random random, int fortune) {
-        return ItemRegistry.material;
+    	if (ConfigHandler.enableFakePlayerMining != true)
+    	return Blocks.cobblestone.getItemDropped(damage, random, fortune);
+    	else
+    	return ItemRegistry.material;	
     }
 
     public int quantityDroppedWithBonus(int fortune, Random random) {
