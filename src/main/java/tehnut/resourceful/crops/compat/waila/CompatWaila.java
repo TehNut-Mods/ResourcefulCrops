af62b57b7c1dde2204b3c8ed53ae83e0454f0b30
Nick <speedynutty68@gmail.com>
2016-01-14 18:53:36 -0800

    Fix crash without Waila

--- src/main/java/tehnut/resourceful/crops/compat/waila/CompatWaila.java
+++ src/main/java/tehnut/resourceful/crops/compat/waila/CompatWaila.java
@@ -1,16 +1,13 @@
 package tehnut.resourceful.crops.compat.waila;
 
-import mcp.mobius.waila.api.IWailaRegistrar;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
-import tehnut.resourceful.crops.api.ModInformation;
-import tehnut.resourceful.crops.block.BlockRCrop;
 import tehnut.resourceful.crops.compat.ICompatibility;
 
 public class CompatWaila implements ICompatibility {
 
     @Override
     public void loadCompatibility() {
-        FMLInterModComms.sendMessage("Waila", "register", "tehnut.resourceful.crops.compat.waila.CompatWaila.callbackRegister");
+        FMLInterModComms.sendMessage("Waila", "register", "tehnut.resourceful.crops.compat.waila.WailaCallbackHandler.callbackRegister");
     }
 
     @Override
@@ -21,10 +18,5 @@ public class CompatWaila implements ICompatibility {
     @Override
     public boolean enableCompat() {
         return true;
-    }
-
-    public static void callbackRegister(IWailaRegistrar registrar) {
-        registrar.registerStackProvider(new ResourcefulCropsDataProvider(), BlockRCrop.class);
-        registrar.addConfig(ModInformation.NAME, "outputStack", true);
     }
 }
---
+++ src/main/java/tehnut/resourceful/crops/compat/waila/WailaCallbackHandler.java
@@ -0,0 +1,13 @@
+package tehnut.resourceful.crops.compat.waila;
+
+import mcp.mobius.waila.api.IWailaRegistrar;
+import tehnut.resourceful.crops.api.ModInformation;
+import tehnut.resourceful.crops.block.BlockRCrop;
+
+public class WailaCallbackHandler {
+
+    public static void callbackRegister(IWailaRegistrar registrar) {
+        registrar.registerStackProvider(new ResourcefulCropsDataProvider(), BlockRCrop.class);
+        registrar.addConfig(ModInformation.NAME, "outputStack", true);
+    }
+}
