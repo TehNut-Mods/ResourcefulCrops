17093dfec3f75032f76301820ab8fd88080b6d2e
Nick <speedynutty68@gmail.com>
2016-03-07 17:43:02 -0800

    Update Neotech compat with IMC system
    Removes Neotech from auto-obtained dependencies. Also cleans out MCMP and Bookshelf.

--- src/main/java/tehnut/resourceful/crops/compat/neotech/CompatNeoTech.java
+++ src/main/java/tehnut/resourceful/crops/compat/neotech/CompatNeoTech.java
@@ -1,6 +1,10 @@
 package tehnut.resourceful.crops.compat.neotech;
 
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
 import tehnut.resourceful.crops.ConfigHandler;
+import tehnut.resourceful.crops.block.BlockRCrop;
+import tehnut.resourceful.crops.registry.BlockRegistry;
 import tehnut.resourceful.repack.tehnut.lib.iface.ICompatibility;
 
 public class CompatNeoTech implements ICompatibility {
@@ -8,7 +12,7 @@ public class CompatNeoTech implements ICompatibility {
     @Override
     public void loadCompatibility(InitializationPhase phase) {
         if (phase == InitializationPhase.PRE_INIT && ConfigHandler.blacklistNeoTechAccelerator)
-            HandlerNeoTech.blacklistBlocks();
+            FMLInterModComms.sendMessage("neotech", "blacklistFertilizer", Block.blockRegistry.getNameForObject(BlockRegistry.getBlock(BlockRCrop.class)).toString());
     }
 
     @Override
