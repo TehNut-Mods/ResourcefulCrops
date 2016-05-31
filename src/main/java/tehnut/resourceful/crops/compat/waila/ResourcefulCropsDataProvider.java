98779e8ae55555e41e5def2edb7b267c6d687531
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-05-30 22:09:27 -0700

    Re-implement Waila compat
    y did i do dat

--- src/main/java/tehnut/resourceful/crops/compat/waila/ResourcefulCropsDataProvider.java
+++ src/main/java/tehnut/resourceful/crops/compat/waila/ResourcefulCropsDataProvider.java
@@ -1,65 +1,63 @@
-//package tehnut.resourceful.crops.compat.waila;
-//
-//import mcp.mobius.waila.api.IWailaConfigHandler;
-//import mcp.mobius.waila.api.IWailaDataAccessor;
-//import mcp.mobius.waila.api.IWailaDataProvider;
-//import net.minecraft.entity.player.EntityPlayerMP;
-//import net.minecraft.item.ItemStack;
-//import net.minecraft.nbt.NBTTagCompound;
-//import net.minecraft.tileentity.TileEntity;
-//import net.minecraft.util.ResourceLocation;
-//import net.minecraft.util.math.BlockPos;
-//import net.minecraft.world.World;
-//import tehnut.lib.util.helper.ItemHelper;
-//import tehnut.resourceful.crops.api.ResourcefulAPI;
-//import tehnut.resourceful.crops.api.base.Seed;
-//import tehnut.resourceful.crops.api.registry.SeedRegistry;
-//import tehnut.resourceful.crops.block.BlockRCrop;
-//import tehnut.resourceful.crops.item.ItemSeed;
-//import tehnut.resourceful.crops.tile.TileRCrop;
-//import tehnut.resourceful.crops.util.Utils;
-//
-//import java.util.List;
-//
-//public class ResourcefulCropsDataProvider implements IWailaDataProvider {
-//
-//    @Override
-//    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
-//
-//        if (accessor.getBlock() instanceof BlockRCrop) {
-//            TileEntity cropTile = accessor.getTileEntity();
-//
-//            if (cropTile != null && cropTile instanceof TileRCrop) {
-//                Seed seed = ResourcefulAPI.SEEDS.getObject(((TileRCrop) cropTile).getSeedName());
-//                if (Utils.isValidSeed(seed)) {
-//                    if (config.getConfig("outputStack"))
-//                        return new ItemStack(seed.getOutput().getItem(), 1, seed.getOutput().getItemDamage());
-//                    else
-//                        return new ItemStack(ItemHelper.getItem(ItemSeed.class), 1, ResourcefulAPI.SEEDS.getId(seed));
-//                }
-//            }
-//        }
-//
-//        return Utils.getInvalidSeed(ItemHelper.getItem(ItemSeed.class));
-//    }
-//
-//    @Override
-//    public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-//        return null;
-//    }
-//
-//    @Override
-//    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-//        return null;
-//    }
-//
-//    @Override
-//    public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-//        return null;
-//    }
-//
-//    @Override
-//    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
-//        return null;
-//    }
-//}
+package tehnut.resourceful.crops.compat.waila;
+
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import tehnut.lib.util.helper.ItemHelper;
+import tehnut.resourceful.crops.api.ResourcefulAPI;
+import tehnut.resourceful.crops.api.base.Seed;
+import tehnut.resourceful.crops.block.BlockRCrop;
+import tehnut.resourceful.crops.item.ItemSeed;
+import tehnut.resourceful.crops.tile.TileRCrop;
+import tehnut.resourceful.crops.util.Utils;
+
+import java.util.List;
+
+public class ResourcefulCropsDataProvider implements IWailaDataProvider {
+
+    @Override
+    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
+
+        if (accessor.getBlock() instanceof BlockRCrop) {
+            TileEntity cropTile = accessor.getTileEntity();
+
+            if (cropTile != null && cropTile instanceof TileRCrop) {
+                Seed seed = ResourcefulAPI.SEEDS.getObject(((TileRCrop) cropTile).getSeedName());
+                if (Utils.isValidSeed(seed)) {
+                    if (config.getConfig("outputStack"))
+                        return new ItemStack(seed.getOutput().getItem(), 1, seed.getOutput().getItemDamage());
+                    else
+                        return new ItemStack(ItemHelper.getItem(ItemSeed.class), 1, ResourcefulAPI.SEEDS.getId(seed));
+                }
+            }
+        }
+
+        return Utils.getInvalidSeed(ItemHelper.getItem(ItemSeed.class));
+    }
+
+    @Override
+    public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    @Override
+    public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
+        return null;
+    }
+}
--- src/main/java/tehnut/resourceful/crops/compat/waila/WailaCallbackHandler.java
+++ src/main/java/tehnut/resourceful/crops/compat/waila/WailaCallbackHandler.java
@@ -1,16 +1,16 @@
-//package tehnut.resourceful.crops.compat.waila;
-//
-//import mcp.mobius.waila.api.IWailaRegistrar;
-//import tehnut.lib.annot.Used;
-//import tehnut.resourceful.crops.api.ModInformation;
-//import tehnut.resourceful.crops.block.BlockRCrop;
-//
-//@Used
-//public class WailaCallbackHandler {
-//
-//    @Used
-//    public static void callbackRegister(IWailaRegistrar registrar) {
-//        registrar.registerStackProvider(new ResourcefulCropsDataProvider(), BlockRCrop.class);
-//        registrar.addConfig(ModInformation.NAME, "outputStack", true);
-//    }
-//}
+package tehnut.resourceful.crops.compat.waila;
+
+import mcp.mobius.waila.api.IWailaRegistrar;
+import tehnut.lib.annot.Used;
+import tehnut.resourceful.crops.api.ModInformation;
+import tehnut.resourceful.crops.block.BlockRCrop;
+
+@Used
+public class WailaCallbackHandler {
+
+    @Used
+    public static void callbackRegister(IWailaRegistrar registrar) {
+        registrar.registerStackProvider(new ResourcefulCropsDataProvider(), BlockRCrop.class);
+        registrar.addConfig(ModInformation.NAME, "outputStack", true);
+    }
+}
